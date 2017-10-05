242c2983b82f207a93f9a2a34449a767ccc975f9
Henry Partridge <hcpartridge@uclmail.net>
2017-10-05 15:49:02 +0100

    Theme updated
     Add left-aligned axis labels and white background

--- R/theme/theme_lab.R
+++ R/theme/theme_lab.R
@@ -3,19 +3,19 @@ theme_lab <- function () {
     theme(
       # add padding to the plot
       plot.margin = unit(rep(0.5, 4), "cm"),
-      # remove the panel background and border
+      # remove the plot, panel background and border
+      plot.background = element_rect(fill = "transparent", colour = NA),
       panel.background = element_blank(),
       panel.border = element_blank(),
-      # add a light grey background
-      plot.background = element_rect(fill = "#f0f0f0", colour = NA), 
       # make the legend and key transparent
       legend.background = element_rect(fill = "transparent", colour = NA),
       legend.key = element_rect(fill = "transparent", colour = NA),
       # add light, dotted major grid lines only
-      panel.grid.major = element_line(linetype = "dotted", colour = "#d9d9d9", size = 0.5),
+      panel.grid.major = element_line(linetype = "dotted", colour = "#757575", size = 0.3),
       panel.grid.minor = element_blank(),
-      # remove the axis ticks
+      # remove the axis ticks and hide axis lines
       axis.ticks = element_blank(),
+      axis.line = element_line(color = "#FFFFFF", size = 0.3),
       # modify the bottom margins of both the title and subtitle
       plot.title = element_text(size = 18, colour = "#757575", hjust = 0, margin = margin(b = 15)),
       plot.subtitle = element_text(size = 12, colour = "#757575", hjust = 0, margin = margin(b = 5)),
@@ -26,6 +26,6 @@ theme_lab <- function () {
       axis.text = element_text(family = "Open Sans", size = 10, colour = "#757575", face = "plain"),
       legend.title = element_text(size = 12, colour = "#757575"),
       legend.text = element_text(size = 10, colour = "#757575"),
-      strip.text = element_text(family = "Open Sans",size = 12, colour = "#757575", face = "plain")
+      strip.text = element_text(family = "Open Sans", size = 12, colour = "#757575", face = "plain")
     )
 }
