5e1172a769a58da455bf97bca630fdc361e995a3
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-01-17 14:42:46 +0000

    Burglary rate using households

--- community_safety/burglary/script.R
+++ community_safety/burglary/script.R
@@ -6,11 +6,9 @@ source("https://github.com/traffordDataLab/assets/raw/master/theme/ggplot2/theme
 # load crime data
 df <- read_csv("data/burglary.csv")
 
-# load ONS mid-year population estimates (2016)
-pop <- read_csv("https://github.com/traffordDataLab/open_data/raw/master/mid-year_pop_estimates_2016/ONS_mid-year_population_estimates_2016.csv") %>% 
-  filter(lad16nm == "Trafford") %>% 
-  select(area_name = wd16nm, total_pop) %>% 
-  add_row(area_name = "Trafford", total_pop = sum(.$total_pop)) # add Trafford total
+# load number of households with at least one resident (Census 2011)
+hlds <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_619_1.data.csv?date=latest&geography=1237320482...1237320496,1237320498,1237320497,1237320499...1237320502,1946157089,1937768449&rural_urban=0&cell=0&measures=20100&select=date_name,geography_name,geography_code,rural_urban_name,cell_name,measures_name,obs_value,obs_status_name") %>% 
+  select(area_code = GEOGRAPHY_CODE, area_name = GEOGRAPHY_NAME, households = OBS_VALUE)
 
 # ------------------------------------------
 
@@ -36,23 +34,24 @@ write_csv(fig1, "outputs/fig1.csv")
 
 # ------------------------------------------
 
-## Trend and seasonality
+## Observed and trend
 count <- df %>% group_by(month) %>% count()
 ts_crime <- ts(count$n, start = c(2014, 12), end = c(2017, 11), frequency = 12)
 ts_decomp = stl(ts_crime, "periodic")
+plot(ts_decomp)
+
 time_series <- data.frame(
   month = seq(as.Date("2014-12-01"), by = "month", length.out = 36),
   Observed = as.vector(ts_crime),
-  Trend = ts_decomp$time.series[,2],
-  Seasonal = ts_decomp$time.series[,1]
+  Trend = ts_decomp$time.series[,2]
 ) %>% gather(type, value, -month) %>% 
-  mutate(type = factor(type, levels = c("Observed", "Trend", "Seasonal")))
+  mutate(type = factor(type, levels = c("Observed", "Trend")))
 fig2 <- time_series
 
 ggplot(data = time_series, aes(month, value)) +
   geom_line(colour = "#fc6721", size = 1) + 
   scale_x_date(date_labels = "%b-%y", date_breaks = "3 month", expand = c(0,0)) +
-  scale_y_continuous(position = "right") +
+  scale_y_continuous(position = "right", limits = c(0, NA)) +
   labs(x = NULL, y = NULL,
        caption = "Source: data.police.uk  |  @traffordDataLab") +
   facet_grid(type ~ ., scales = "free", switch = "y") +
@@ -74,8 +73,8 @@ fig3 <- df %>%
   count() %>% 
   ungroup() %>% 
   add_row(area_name = "Trafford", n = sum(.$n)) %>% # add Trafford total
-  left_join(., pop, by = "area_name") %>% 
-  mutate(rate = round((n/total_pop)*1000,1)) %>% 
+  left_join(., hlds, by = "area_name") %>% 
+  mutate(rate = round((n/households)*1000,1)) %>% 
   arrange(desc(rate)) %>%
   mutate(area_name = factor(area_name, levels = area_name))
 
@@ -83,7 +82,7 @@ ggplot(fig3, aes(rate, area_name)) +
   geom_segment(aes(x = 0, y = area_name, xend = rate, yend = area_name), color = "#f0f0f0") +
   geom_point(colour = "#fc6721", size = 4) +
   geom_text(aes(label = rate, fontface = "bold"), color = "white", size = 1.5) + 
-  labs(x = "Crimes per 1,000 residents", y = NULL,
+  labs(x = "Crimes per 1,000 households", y = NULL,
        title = NULL,
        caption = "Source: data.police.uk  |  @traffordDataLab") +
   theme_lab() + 
@@ -93,3 +92,17 @@ ggplot(fig3, aes(rate, area_name)) +
 ggsave(file = "outputs/fig3.svg", width = 6, height = 6)
 ggsave(file = "outputs/fig3.png", width = 6, height = 6)
 write_csv(fig3, "outputs/fig3.csv")
+
+# Table
+tab1 <- df %>% 
+  filter(month == "2017-11-01") %>% 
+  group_by(area_name) %>% 
+  count() %>% 
+  ungroup() %>% 
+  left_join(., hlds, by = "area_name") %>% 
+  mutate(rate = round((n/households)*1000,1)) %>% 
+  select(Ward = area_name, Count = n, Rate = rate) %>% 
+  add_row(Ward = "Trafford", Count = sum(.$Count), Rate = round((Count/94484)*1000,1)) %>% 
+  add_row(Ward = "Greater Manchester", Count = NA, Rate = round((NA/1128066)*1000,1))
+View(tab1)
+write_csv(tab1, "outputs/tab1.csv")
