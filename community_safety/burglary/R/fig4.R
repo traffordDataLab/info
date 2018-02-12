fdbbb9c78c6eaabfbec96f451a2297ab9caea589
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2018-02-12 12:46:13 +0000

    Added ward abbreviations to maps

--- community_safety/burglary/R/fig4.R
+++ community_safety/burglary/R/fig4.R
@@ -15,7 +15,13 @@ households <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_619_1.data.
 
 # load geospatial data  ---------------------------
 sf <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/ward/2017/trafford_ward_generalised.geojson") %>% 
-  st_as_sf(crs = 4326, coords = c("long", "lat"))
+  st_as_sf(crs = 4326, coords = c("long", "lat")) %>% 
+  select(-lat, -lon)
+
+lookup <- read_csv("https://github.com/traffordDataLab/spatial_data/raw/master/lookups/ward_abbreviations.csv") %>% 
+  select(-area_name)
+
+sf <- left_join(sf, lookup, by = "area_code")
 
 # manipulate data ---------------------------
 results <- df %>% 
@@ -34,6 +40,7 @@ sf_df <- left_join(sf, results, by = "area_code") # merge with ward attribute ta
 # plot data ---------------------------
 ggplot(sf_df) +
   geom_sf(aes(fill = lq), colour = "white") +
+  geom_text(aes(lon, lat, label = area_abbr), size = 2.5, color = "#212121") +
   scale_fill_gradientn(colours = c("#feedde","#fdbe85","#fd8d3c","#e6550d","#a63603"), 
                        na.value = "#f0f0f0",
                        guide = guide_legend(keyheight = unit(3, units = "mm"), 
