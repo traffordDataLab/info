0167f79fdb1ff83cce5a62065e3961514713a521
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2018-02-15 14:44:44 +0000

    Template for R scripts

---
+++ template.R
@@ -0,0 +1,16 @@
+## [theme name]: [figure title] ##
+
+# load R packages  ---------------------------
+
+# load Lab's ggplot2 theme  ---------------------------
+source("https://github.com/traffordDataLab/assets/raw/master/theme/ggplot2/theme_lab.R")
+
+# load data  ---------------------------
+
+# load geospatial data  ---------------------------
+
+# manipulate data ---------------------------
+
+# plot data ---------------------------
+
+# save plot / data  ---------------------------
