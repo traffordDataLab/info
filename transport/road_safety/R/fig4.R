bca96ffac7a0a3acba47edd912052a378f5299c9
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2018-04-23 11:42:51 +0100

    Fixed legend on Fig 4

--- transport/road_safety/R/fig4.R
+++ transport/road_safety/R/fig4.R
@@ -28,21 +28,22 @@ collisions <- df %>%
 dates <- data_frame(month_year = seq(as.Date('2016-01-01'), as.Date('2016-12-01'), by = "1 month"))
 
 results <- left_join(dates, collisions, by = "month_year") %>% 
-  gather(mode, n, -month_year) 
+  gather(mode, n, -month_year) %>% 
+  mutate(n = replace(n, is.na(n), 0))
 
 # plot data  ---------------------------
 p <- ggplot(results, aes(x = month_year, y = mode, fill = n)) + 
   geom_tile(color = "white", size = 0.4) + 
-  scale_fill_viridis_c(name = "Number of collisions", 
-                       direction = -1, 
-                       na.value = "grey93", 
-                       breaks = scales::pretty_breaks(),
-                       limits = c(0, max(results$n)),
-                       guide = guide_legend(keyheight = unit(3, units = "mm"), 
-                                            keywidth=unit(12, units = "mm"), 
-                                            label.position = "bottom", 
-                                            title.position = 'top', 
-                                            nrow = 1)) +
+  scale_fill_viridis(name = "Number of collisions", 
+                     direction = -1, 
+                     na.value = "grey93", 
+                     breaks = c(1, 3, 5, 7, 9, 11),
+                     limits = c(1, max(results$n)),
+                     guide = guide_legend( keyheight = unit(3, units = "mm"),
+                                           keywidth=unit(12, units = "mm"), 
+                                           label.position = "bottom", 
+                                           title.position = 'top', 
+                                           nrow = 1)) +
   labs(x = NULL, y = NULL, fill = NULL, title = NULL, subtitle = NULL) +
   facet_grid(mode ~ ., scales = "free") +
   scale_x_date(date_labels = "%b", date_breaks = "1 month", expand = c(0,0)) +
